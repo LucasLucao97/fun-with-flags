138f121fce74d7982cdf8f3706eac829b6a8b708
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-12-06 12:41:37 -0300

    feat :sparkles: (app) Feature footer and header components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return(
+    <footer className="py-6 mb-8">
+        <p className="text-center">
+          Made by <a href="https://github.com/LucasLucao97">@LucasBarbosa</a>
+        </p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return(
+    <header className="py-6 mb-8">
+      <h1 className="text-3x1 font-bold">
+        Fun with flags
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
