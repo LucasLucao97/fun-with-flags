5d2ae601eeb7fe4530e7339d36fb1beb34469afe
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-12-13 13:02:41 -0300

    feat :sparkles: (components) Feature Component Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Footer, Header } from "./components";
+import { Card, Footer, Header } from './components';
+import Grid from './components/Grid';
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className="flex-1">
-        This is the main
+        <Grid>
+          {countries.map(({ id, country, capital, region, population }) =>
+            <Card
+              key={id}
+              country={country}
+              capital={capital}
+              region={region}
+              population={population}
+            />
+          )}
+        </Grid>
       </main>
       <Footer />
     </>
