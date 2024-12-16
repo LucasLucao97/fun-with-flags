b8361e8e89dabf7bf461843be0335460562a21b8
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-12-15 22:33:28 -0300

    feat :sparkles: (page and components) Feature a call to API

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
-import { Card, Footer, Header } from './components';
-import Grid from './components/Grid';
+'use client'
+import { useEffect, useState } from 'react';
+import { Card, Footer, Grid, Header } from './components';
 
 export default function Home() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await fetch("https://restcountries.com/v3.1/all")
+      const data = await response.json()
+      setCountries(data)
+    }
+
+    fetchCountries()
+  }, [])
+
   return (
     <>
       <Header />
       <main className="flex-1">
         <Grid>
-          {countries.map(({ id, country, capital, region, population }) =>
+          {countries.map(({ id, name, capital, region, population }) =>
             <Card
               key={id}
-              country={country}
+              country={name.common}
               capital={capital}
               region={region}
               population={population}
